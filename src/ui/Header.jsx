3d26e7b6635f6dac465a13c1d553a889484438da
Abi MH <abimash6@gmail.com>
2024-07-12 19:11:26 +0700

    initial commit

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Pizza React Fast Co.</Link>
+
+      <p>Abi</p>
+    </header>
+  );
+}
+
+export default Header;
